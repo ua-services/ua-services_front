126dff683b0dcc79dc25334ebd26f57167a8a613
Mykola Vasylkiv <mvasilkiv21@gmail.com>
2020-04-12 18:46:43 +0300

    add map

--- src/react/pages/profile/UserProfile.js
+++ src/react/pages/profile/UserProfile.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Button } from "antd";
+import { Menu } from 'antd';
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import { connect } from "react-redux";
+
+const { SubMenu } = Menu;
 
 
 const UserProfile = props => {
-  const { user } = props;
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+
+  const getLocationCB = ({ coords }) => {
+    const { latitude, longitude } = coords;
+
+    setLat(latitude);
+    setLng(longitude);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(getLocationCB);
+  }, []);
+
+
+  const position = [lat, lng];
 
   return (
-    <div>
-      {user.first_name}
-    </div>
+    <LeafletMap center={position} zoom={15}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br/> Easily customizable.
+        </Popup>
+      </Marker>
+    </LeafletMap>
   )
 };
 
